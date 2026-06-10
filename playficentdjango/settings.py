fc8660c211ffdd7f2a6341c8981be7414d0e30bc
Raj Maharjan <162679011+1RajMaharjan@users.noreply.github.com>
2026-06-10 21:22:15 +0545

    Fix render settings

--- playficentdjango/settings.py
+++ playficentdjango/settings.py
@@ -154,8 +154,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # ==================================================
 
 CORS_ALLOWED_ORIGINS = [
-    "https://playficentdjango-1.onrender.com",
-    "http://127.0.0.1:5173",
+     "playficentdjango.onrender.com",
+    "playficentdjango-1.onrender.com",
 
     "http://localhost:5174",
     "http://127.0.0.1:5174",
@@ -168,18 +168,18 @@ CORS_ALLOW_CREDENTIALS = True
 # ==================================================
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://playficentdjango-1.onrender.com",
-    "http://127.0.0.1:5173",
+    "playficentdjango.onrender.com",
+    "playficentdjango-1.onrender.com",
 
     "http://localhost:5174",
     "http://127.0.0.1:5174",
 ]
 
-SESSION_COOKIE_SAMESITE = "Lax"
-CSRF_COOKIE_SAMESITE = "Lax"
+SESSION_COOKIE_SAMESITE = "None"
+CSRF_COOKIE_SAMESITE = "None"
 
-SESSION_COOKIE_SECURE = False
-CSRF_COOKIE_SECURE = False
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
 
 # ==================================================
 # CACHE
